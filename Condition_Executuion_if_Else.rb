039eadcc87e07854e407895c5df31a7ebb241f32
Siddhartha <masterwww03@gmail.com>
2020-12-09 20:47:33 +0530

    if & else

--- Condition_Executuion_if_Else.rb
+++ Condition_Executuion_if_Else.rb
@@ -1,1 +1,20 @@
 ## Condition Executuion if & else
+# if-else conditional execution
+
+# Weather = "Raining"
+    
+# if Weather == "Raining"
+#     puts "I am staying home"
+# else
+#     puts "I am going out for a walk"
+# end
+
+#####
+x = 15
+if x < 0
+    puts "Negative Number"
+elseif x > 0
+    puts "Positive Number"
+else
+    puts "Zero"
+end
