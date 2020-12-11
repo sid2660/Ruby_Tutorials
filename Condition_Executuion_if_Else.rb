8dcbfa7d627579c80470bb6a86ede705301b7e3e
Siddhartha <masterwww03@gmail.com>
2020-12-11 13:16:44 +0530

    ..

--- Condition_Executuion_if_Else.rb
+++ Condition_Executuion_if_Else.rb
@@ -10,14 +10,14 @@
 # end
 
 #####
-x = 15
-if x < 0
-    puts "Negative Number"
-elsif x > 0
-    puts "Positive Number"
-else
-    puts "Zero!"
-end
+# x = 15
+# if x < 0
+#     puts "Negative Number"
+# elsif x > 0
+#     puts "Positive Number"
+# else
+#     puts "Zero!"
+# end
 
 #####################################################################################
 =begin
@@ -33,3 +33,34 @@ end
 | !=    |               |               | Not Equal|
 '--------------------------------------------------'
 =end
+
+#################################################################
+
+## Conditional Expression - ||(OR)
+
+# account_balance = 1000000
+# age = 75
+# if account_balance == 1000000 || age == 70
+# 	puts "You can retire now, Good Luck!"
+# else
+# 	puts "You still need to save money for your retirement."
+# end
+
+
+####################################################################
+
+### Conditional Expression - && (AND)
+
+grade = 4.1
+
+if grade >= 4.0
+	puts "You are awesom student."
+elsif grade >= 3.5 && grade < 4.0
+	puts "You are a grade student"
+elsif grade >= 3.0 && grade < 3.5
+	puts "You are average student"
+elsif grade > 1.5 && grade < 3.0
+	puts "You need to do better."
+else
+	puts "You failed the class."
+end
