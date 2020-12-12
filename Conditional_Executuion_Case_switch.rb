bfe53faf906f95b785c9b11c3b14a66a606a7f28
Siddhartha <masterwww03@gmail.com>
2020-12-12 19:13:15 +0530

    ..

--- Conditional_Executuion_Case_switch.rb
+++ Conditional_Executuion_Case_switch.rb
@@ -1,17 +1,32 @@
 ## Conditional Execution Case
 
 
-grade = "A"
+# grade = "A"
 
-case(grade)
-	when "A+","A","A-"
-		puts "Great Student"
-	when "B+","B","B-"
-		puts "Good Student"
-	when "C+","C","C-"
-		puts "Poor Student"
-	when "D+","D","D-"
-		puts "very Poor Student"
+# case(grade)
+# 	when "A+","A","A-"
+# 		puts "Great Student"
+# 	when "B+","B","B-"
+# 		puts "Good Student"
+# 	when "C+","C","C-"
+# 		puts "Poor Student"
+# 	when "D+","D","D-"
+# 		puts "very Poor Student"
+# 	else
+# 		puts "Failed"
+# end
+
+##########
+
+price = 15000
+
+case(price)
+	when 10000
+		puts "1 BHK"
+	when 15000
+		puts "2 BHK"
+	when 20000 
+		puts "3 BHK"
 	else
-		puts "Failed"
+		puts "4 BHK"
 end
