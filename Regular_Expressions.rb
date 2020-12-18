d4c591bcbde159d2f917490258b194421eee02ac
Siddhartha <masterwww03@gmail.com>
2020-12-18 16:04:06 +0530

    ..

---
+++ Regular_Expressions.rb
@@ -0,0 +1,15 @@
+
+#____________________________ Regular Expressions _____________________________________#
+#--------------------------------------------------------------------------------------#
+
+
+#_____________________ # _________________________ # _________________________________ # ______________________
+
+#	=> Regular Expression (regex or regexp for short) is a special text string
+#	   for describing a search pattern.
+#
+#	=> Regex pattern are normally described inside the //
+
+#____________________________ Example: ______________________
+
+# => Wildcard notations such as /*.txt/ to find all text files in a file manager.
