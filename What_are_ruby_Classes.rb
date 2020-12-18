bf5ff11a354377b01c0aa603280c390d2b7f4661
Siddhartha <masterwww03@gmail.com>
2020-12-18 15:15:23 +0530

    What are Ruby Classes

--- What_are_ruby_Classes.rb
+++ What_are_ruby_Classes.rb
@@ -2,7 +2,7 @@
 #___________________________________ What are Ruby Classes _______________________________#
 #-----------------------------------------------------------------------------------------#
 
-
+#____________________________________________________________________________#
 ##_____________________________ Classes  ____________________________________
 
 #	     => Class is a specification or a blue print (set of instructions) of how
@@ -22,6 +22,79 @@
 #   Animal - Dog, Cat, Giraffe, Monkey
 #	Vehicle - Car, Bicycle, Motorbike, SUV
 #	Ford - Focus, F-150, Mustang, Explorer, Fiesta
+#	Dog - Corgi, German Shepherd, Labrador
+#	Honda - Accord, Civic, Pilot, CR-V
+
+###____________________________###______________###____________###___________###_______________###
+
+#________________________________________________________________________________________#
+#____________________________________ Classes Contd.. ___________________________________
+
+
+#                                       Pet
+#                                        ^
+#                                        |
+#                                       Dog
+#                                        ^
+#                         _______________|_______________
+#                         ^	             ^               ^
+#                         |              |               |
+#                       Hound         Terrier          Poodle
+#                         ^              ^               ^
+#                         |              |               |
+#                       Beagle         Weish          Toy Poddle
+#                                      Terrer
+
+#_________________________________________________________________________________________#
+
+class Dog
+	def name_of_dog(name)
+		puts "I am #{name}"
+	end
+	
+	def bark
+		puts "I am barking"
+	end
+	
+	def eat
+		puts "I am eating"
+	end
+	
+	def sleep
+		puts "I am sleeping"
+	end
+	
+	def play 
+		puts "I am playing"
+	end
+end
+
+
+# Create an object from the class.
+# Classname.new
+
+# Create an object called corgi
+
+corgi = Dog.new
+
+# Call name of dog method
+
+corgi.name_of_dog("Jack")
+corgi.bark
+corgi.eat
+corgi.sleep
+
+
+puts "______________________________"
+# Create another object
+
+labrador = Dog.new
+
+labrador.name_of_dog("Max")
+labrador.bark
+labrador.eat
+labrador.sleep
+
 
 
 
