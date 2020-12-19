fda8d0b3edc7c0cdeecc525c29e5dfe3bbd7d947
Siddhartha <masterwww03@gmail.com>
2020-12-19 13:31:15 +0530

    ..

--- Regular_Expressions.rb
+++ Regular_Expressions.rb
@@ -13,3 +13,23 @@
 #____________________________ Example: ______________________
 
 # => Wildcard notations such as /*.txt/ to find all text files in a file manager.
+
+
+#.___________________________________________________________________________________.
+#| RegEx Character                            Meaning                                |
+#|                                                                                   |
+#|         ^					           Begining of string                        |
+#|         $					           End of String                             |
+#|         .					           Any Character                             |
+#|         *					           Match 0 or more times                     |
+#|         +					           Match 1 or more times                     |
+#|         ?					           Match 0 or 1 times                        |
+#|         |					           alternative                               |
+#|         ()					           Grouping                                  |
+#|         []					           Match any string inside the square bracket|
+#|         {}					           Repetition modifier                       |   
+#|         \					           Escape(turn RegEx in plain Character)     |
+#'-----------------------------------------------------------------------------------'
+
+
+
