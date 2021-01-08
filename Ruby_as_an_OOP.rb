4971a1d7a4d37f5105604ceeff99195f963f1480
Siddhartha <masterwww03@gmail.com>
2021-01-08 22:38:25 +0530

    ..

--- Ruby_as_an_OOP.rb
+++ Ruby_as_an_OOP.rb
@@ -49,22 +49,22 @@
 
 ##-----------------------------------------------------------------------------
 
-class Student
-	attr_reader :cust_id
-	attr_accessor :cust_name, :cust_addr
-	def initialize(id,name,addr)
-		@cust_id = id
-		@cust_name = name
-		@cust_addr = addr
-	end
-end
+# class Student
+# 	attr_reader :cust_id
+# 	attr_accessor :cust_name, :cust_addr
+# 	def initialize(id,name,addr)
+# 		@cust_id = id
+# 		@cust_name = name
+# 		@cust_addr = addr
+# 	end 
+# end
 
 # ----- Create Object ------
 
-s1 = Student.new(2741, "George", "593 Baltimore Avenue, Boston MA 02112")
-puts s1.cust_id
-puts s1.cust_name
-puts s1.cust_addr
+# s1 = Student.new(2741, "George", "593 Baltimore Avenue, Boston MA 02112")
+# puts s1.cust_id
+# puts s1.cust_name
+# puts s1.cust_addr
 
 #------- Not able to change the id because it is read only ---------
 
@@ -72,10 +72,117 @@ puts s1.cust_addr
 
 #------- Can Change the name and address --------
 
-s1.cust_name = "Danny"
-s1.cust_addr = "486 Houston Avenue, Chicago, IL 20018"
+# s1.cust_name = "Danny"
+# s1.cust_addr = "486 Houston Avenue, Chicago, IL 20018"
 
-puts s1.cust_name
-puts s1.cust_addr
+# puts s1.cust_name
+# puts s1.cust_addr
 
 
+#_______________________________________________________________________________________________________________
+#---------------------------------------------  Inheritance ----------------------------------------------------
+
+# > Relation between two classes where a child class inherits all features of its parent class.
+# > Methods from the parent can be overridden in the child and new logic can be added.
+
+
+#									       Person
+#									         |
+#								             |
+#					      .---------------------------------------------.
+#					      |                                             |
+#					      |                                          Employee
+#                       Student                                        / \
+#					      /\                                          / | \
+#                        /  \                                        /  |  \
+#                       /    \                                      /   |   \
+#                      /      \                                    /    |    \
+#                     /        \                                  /     |     \
+#                    IT      Science                        Teacher    HR     Utility
+
+#_____________________________________________________________________________________________________________
+
+# #---------- Parent Class
+# class Animal
+#     def breath
+#         puts "Enhale and Exhale"
+#     end
+# end
+
+# #----------- Child Class
+# class Dog < Animal
+#     def brak 
+#         puts "Barking"
+#     end
+# end
+
+# #---- Create an Object
+
+# d = Dog.new
+# d.breath
+# d.bark
+
+
+
+
+
+
+
+
+
+
+#-------- Parent/Base Class
+
+class Box
+	def initalize(w,h)
+		@width = w
+		@height = h
+	end
+	
+	def display_box_name
+		puts "I am box class"
+	end
+end
+
+#-------- Child Class
+
+class SmallBox < Box
+	def print_area
+		@area = @width * @height
+		puts "Area of the small box is #{@area}"
+	end
+	
+	def display_price
+		puts "My price is 1200"
+	end
+end
+
+#--------  BigBox
+
+class BigBox < Box
+	def initialize(v,w,h)
+		@volume = v
+		@width = w
+		@height = h
+	end
+	
+	def print_area
+		@area = @width * @height * @volume
+		puts "Big box area is #{@area}"
+	end
+end
+
+
+p "----------- Box ----------------"
+b1 = Box.new
+b1.display_box_name
+
+p "------------ Small box -----------"
+b2 = SmallBox.new
+b2.display_box_name
+b2.display_price
+
+p"-------------- Big Box ------------"
+b3 = BigBox.new(10,20,30)
+b3.display_box_name
+b3.print_area  
